55245973854b5ea486fab6ecf687f1efb0441bbe
MO Mohammed <340452@student.mboutrecht.nl>
2025-11-01 21:40:51 +0100

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/'   // vervang <repo-naam> door je echte GitHub-repo
+  base: '/Mo.github.io/'   // vervang <repo-naam> door je echte GitHub-repo
 })
