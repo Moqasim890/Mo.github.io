e21e2e778a66009dd5f1dd8d47397603dbcdb38d
MO Mohammed <340452@student.mboutrecht.nl>
2025-11-02 02:41:52 +0100

    Update deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './' // This is important for GitHub Pages deployment
+  base: '/Mo.github.io/' // Change this to match your repository name
 })
 //fixed deployment issue with gh pages by adding base property
 //https://vitejs.dev/guide/static-deploy.html#github-pages
