ea17a94d9eb57ebaf932d939a5f20c23b412875b
MO Mohammed <340452@student.mboutrecht.nl>
2025-11-02 02:24:05 +0100

    fix vite config

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+// src/main.jsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -6,7 +7,7 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Mo.github.io">
       <App />
     </BrowserRouter>
   </StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,4 @@ export default defineConfig({
 })
 //fixed deployment issue with gh pages by adding base property
 //https://vitejs.dev/guide/static-deploy.html#github-pages
-//also make sure to set "homepage" in package.json to "https://<your-github-username>.github.io/<repo-name>/"
+//also make sure to set "homepage" in package.json to "https://<your-github-username>.github.io/<repo-name>/"Q
